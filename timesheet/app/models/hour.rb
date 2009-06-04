eb0ff3ef035e0a179f8813bd7c5bbbbc0ad01bb6
sintax <sintax@bddb56bf-75d5-424c-8a92-fb5ecad608ce>
2009-06-04 08:18:52 +0000

    Also allow entry in hh:mm format
    git-svn-id: http://172.16.0.11/svn/timesheet/trunk@28 bddb56bf-75d5-424c-8a92-fb5ecad608ce

--- timesheet/app/models/hour.rb
+++ timesheet/app/models/hour.rb
@@ -1,3 +1,32 @@
 class Hour < ActiveRecord::Base
   belongs_to :timesheet
+
+  def normal=(value)
+    self[:normal] = convert(value)
+  end
+
+
+  def rate2=(value)
+    self[:rate2]= convert(value)
+  end
+
+  def rate3=(value)
+    self[:rate3]= convert(value)
+  end
+
+  def travel=(value)
+    self[:travel]= convert(value)
+  end
+
+  protected
+
+    def convert(value)
+      if value.to_s.include? ':'
+        v= value.to_s.split(':')
+        (v[0].to_f + v[1].to_f/60.0).to_f
+      else
+        value
+      end
+    end
+
 end
