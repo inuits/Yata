02986c4a65e8e568c58d950ea4569b1a9b0be56c
Julien Pivotto <roidelapluie@inuits.eu>
2011-07-28 10:18:04 +0200

    prepare db
    Signed-off-by: Julien Pivotto <roidelapluie@inuits.eu>

---
+++ db/migrate/20110727164818_create_projects.rb
@@ -0,0 +1,18 @@
+class CreateProjects < ActiveRecord::Migration
+  def self.up
+    create_table :projects do |t|
+      t.integer :customer_id, :null => false
+      t.string :shortname, :limit => 10
+      t.string :name, :null => false
+
+
+      t.timestamps
+    end
+    add_column :timesheets, :project, :integer, :null => false
+  end
+
+  def self.down
+    drop_table :projects
+    remove_column :timesheets, :project
+  end
+end
