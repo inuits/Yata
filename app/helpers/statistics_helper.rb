84dc7dd3fb4805716e7ab8afa378a371cd4c4d65
Julien Pivotto <roidelapluie@inuits.eu>
2011-08-31 16:38:10 +0200

    first version of statistics_helpers

---
+++ app/helpers/statistics_helper.rb
@@ -0,0 +1,19 @@
+module StatisticsHelper
+
+  def monthnames
+    [["Jan", 1], ["Feb", 2], ["Mar", 3], ["Apr", 4], ["May", 5], ["Jun", 6], ["Jul", 7], ["Aug", 8], ["Sep", 9], ["Oct", 10], ["Nov", 11], ["Dec", 12]]
+  end
+
+  def monthname(num)
+    Date::MONTHNAMES[num]
+  end
+
+  def dhm(hours)
+    if (hours%8) != 0
+      (hours/8).to_i.to_s + "d+"
+    else
+      (hours/8).to_i.to_s + "d"
+    end
+  end
+
+end
