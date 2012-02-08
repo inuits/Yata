b8a41903dcf804256e77f2a8be69bdc5d7dd1a7c
Julien Pivotto <roidelapluie@inuits.eu>
2012-02-08 14:10:27 +0100

    remove old checks

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,7 +1,5 @@
 class Project < ActiveRecord::Base
   validates_presence_of :name, :shortname
-  validates_numericality_of :days, :only_integer => false, :greater_than_or_equal_to => 0, :allow_blank => true
-  validates_numericality_of :cost, :only_integer => false, :greater_than_or_equal_to => 0, :allow_blank => true
 
   has_many :timesheet
   belongs_to :customer
