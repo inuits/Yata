a7f3c59dcd0b171b5b8805fc81c14e5a9139b9c0
Julien Pivotto <roidelapluie@inuits.eu>
2012-02-08 12:05:29 +0100

    Revert "some improvements of projects"
    This reverts commit d6174f9630657d7d70b62ef07dcb132ac802079c.

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,8 +1,7 @@
 class Project < ActiveRecord::Base
   validates_presence_of :name, :shortname
   validates_numericality_of :days, :only_integer => false, :greater_than_or_equal_to => 0, :allow_blank => true
-  validates_numericality_of :total_cost, :only_integer => false, :greater_than_or_equal_to => 0, :allow_blank => true
-  validates_numericality_of :cost_per_day, :only_integer => false, :greater_than_or_equal_to => 0, :allow_blank => true
+  validates_numericality_of :cost, :only_integer => false, :greater_than_or_equal_to => 0, :allow_blank => true
 
   has_many :timesheet
   belongs_to :customer
