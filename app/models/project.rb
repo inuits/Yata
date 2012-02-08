28d19d17956285dcd5a97d021a2488d83afed4de
Julien Pivotto <roidelapluie@inuits.eu>
2012-02-08 15:55:44 +0100

    add infos on projects

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,10 @@
 class Project < ActiveRecord::Base
   validates_presence_of :name, :shortname
-
+  validates_numericality_of :total_price, :only_integer => false, :greater_than_or_equal_to => 0, :allow_blank => true
+  validates_numericality_of :price_per_day, :only_integer => false, :greater_than_or_equal_to => 0, :allow_blank => true
+  validates_numericality_of :duration, :only_integer => false, :greater_than_or_equal_to => 0, :allow_blank => true
+  validates_numericality_of :duration, :only_integer => false, :greater_than_or_equal_to => 0, :allow_blank => true
   has_many :timesheet
   belongs_to :customer
+
 end
