f5134e91d43ed0cdfdeb0e14e63edb4966aeafee
Julien Pivotto <roidelapluie@inuits.eu>
2012-09-20 20:31:48 +0200

    update authusers_controller: add ability to edit user

--- app/controllers/authusers_controller.rb
+++ app/controllers/authusers_controller.rb
@@ -18,6 +18,11 @@ class AuthusersController < ApplicationController
   def new
   end
 
+  # GET /authusers/1
+  def show
+    @authuser = Authuser.find(params[:id])
+  end
+
   # GET /authusers/1/toggleadmin
   def toggleadmin
     if logged_in? and current_authuser.admin
@@ -29,6 +34,38 @@ class AuthusersController < ApplicationController
         end
       @authuser.save()
       redirect_to(authusers_path)
+    else
+      redirect_back_or_default('/')
+    end
+  end
+
+  # GET /authusers/edit
+  def edit
+    if logged_in? and current_authuser.admin
+      @authuser = Authuser.find(params[:id])
+    elsif params[:id] == current_authuser.id
+      @authuser = current_authuser
+    else
+      redirect_back_or_default('/')
+    end
+  end
+
+
+  # PUT /authuser/1
+  # PUT /authuser/1.xml
+  def update
+    if logged_in? and current_authuser.admin or params[:id] == current_authuser.id
+      @authuser = Authuser.find(params[:id])
+      respond_to do |format|
+        if @authuser.update_attributes(params[:authuser])
+          flash[:notice] = 'Your preferences were successfully updated.'
+          format.html { redirect_to(@authuser) }
+          format.xml  { head :ok }
+        else
+          format.html { render :action => "edit" }
+          format.xml  { render :xml => @authuser.errors, :status => :unprocessable_entity }
+        end
+      end
     else
       redirect_back_or_default('/')
     end
