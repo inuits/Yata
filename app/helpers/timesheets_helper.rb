1e47f8e875a22ec655c383653f5433c2c3856d5f
Karl Vogel <karl.vogel@inuits.be>
2014-05-19 20:05:27 +0200

    Only allow editing of a timesheet upto the 15th of the next month.

--- app/helpers/timesheets_helper.rb
+++ app/helpers/timesheets_helper.rb
@@ -12,4 +12,14 @@ module TimesheetsHelper
     (hours/8).to_i.to_s + "d " + (hours%8).to_i.to_s.rjust(2, '0') + ":" + ((hours*60)%60).to_i.to_s.rjust(2, '0') 
   end
 
+  def allow_edit(timesheet)
+    return true if current_authuser.admin
+
+    # allow editing 1 month + 15 days after timesheet date
+    # convert to numeric value to be able to compare, just count
+    # 31 per month, real month length doesnt matter
+    max_edit_date  = timesheet.year*12*31+(timesheet.month+1)*31+15
+    cur = Time.now.year*12*31 + Time.now.month*31 + Time.now.day
+    cur <= max_edit_date
+  end
 end
